82c1ceacd4da02ce4dfc42a60da695b9772cece5
Ravi Terala <terala@users.noreply.github.com>
2024-05-08 09:36:07 -0700

    Add messageID and validation tests for webhook

--- src/webhook/main.py
+++ src/webhook/main.py
@@ -8,11 +8,19 @@ app = FastAPI()
 StrictMSISDN = Annotated[str, Strict()]
 
 
-class SMSPostBody(BaseModel):
-    msisdn: str = Field(min_length=11, max_length=11, pattern="^\\d{11}$")
-    text: str = Field(min_length=1)
+class SMS(BaseModel):
+    msisdn: str = Field(
+        alias="msisdn", min_length=11, max_length=11, pattern="^\\d{11}$"
+    )
+    text: str = Field(alias="text", min_length=1)
+    messageId: str = Field(alias="messageID", min_length=5, max_length=64)
 
 
 @app.post("/webhook")
-async def webhook(sms: SMSPostBody):
-    return {"message": "Hello, World!", "msisdn": sms.msisdn, "text": sms.text}
+async def webhook(sms: SMS):
+    return {
+        "message": "Hello, World!",
+        "msisdn": sms.msisdn,
+        "text": sms.text,
+        "message_ID": sms.messageId,
+    }
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,6 @@
+from fastapi.testclient import TestClient
 import pytest
+from http import HTTPStatus
 from assertpy import assert_that
 
 
@@ -6,8 +8,46 @@ def test_sms_callback():
     pass
 
 
-def test_webhook(client):
+def test_webhook_valid_sms_request(client: TestClient):
     resp = client.post(
-        "/webhook", json={"msisdn": "11234567890", "text": "Hello, World!"}
+        "/webhook",
+        json={
+            "msisdn": "11234567890",
+            "text": "Hello, World!",
+            "messageID": "1234567890",
+        },
     )
-    assert_that(resp.status_code).is_equal_to(200)
+    assert_that(resp.status_code).is_equal_to(HTTPStatus.OK)
+
+
+@pytest.mark.parametrize(
+    "name,json,expected_status_code",
+    [
+        ("Short msisdn", {"Msisdn": "2342"}, HTTPStatus.UNPROCESSABLE_ENTITY),
+        (
+            "msisdn too long",
+            {"Msisdn": "234215487544"},
+            HTTPStatus.UNPROCESSABLE_ENTITY,
+        ),
+        (
+            "msisdn not a valid phone number",
+            {"Msisdn": "234abdwer215487544"},
+            HTTPStatus.UNPROCESSABLE_ENTITY,
+        ),
+        ("Empty msisdn", {"Msisdn": ""}, HTTPStatus.UNPROCESSABLE_ENTITY),
+        ("Missing msisdn", {"text": "something"}, HTTPStatus.UNPROCESSABLE_ENTITY),
+        ("Empty text", {"text": ""}, HTTPStatus.UNPROCESSABLE_ENTITY),
+        ("Empty messageID", {"messageID": ""}, HTTPStatus.UNPROCESSABLE_ENTITY),
+        ("MessageID only", {"messageID": "123"}, HTTPStatus.UNPROCESSABLE_ENTITY),
+        (
+            "Valid messageID, missing other fields",
+            {"messageID": "12345678901234567890"},
+            HTTPStatus.UNPROCESSABLE_ENTITY,
+        ),
+    ],
+)
+def test_sms_request_bad_data(
+    client: TestClient, name: str, json: dict, expected_status_code: HTTPStatus
+):
+    resp = client.post("/webhook", json=json)
+    assert_that(resp.status_code).is_equal_to(expected_status_code)
