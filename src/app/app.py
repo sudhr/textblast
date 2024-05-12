b0e4a58625185aa733fc99971471c493d738335a
Ravi Terala <terala@users.noreply.github.com>
2024-05-12 15:00:00 -0700

    App page - starter

--- src/app/app.py
+++ src/app/app.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, FastAPI, Response, status
 from pydantic.types import Strict
 from sqlalchemy.orm import Session
 
-from .routers import webhook
+from .routers import webhook, index
 
 import db
 from .schemas import SMS
@@ -11,4 +11,6 @@ from .schemas import SMS
 app = FastAPI()
 StrictMSISDN = Annotated[str, Strict()]
 
+# Register Routers
 app.include_router(webhook.router)
+app.include_router(index.router)
