ff26fa56d76fde9672ae16a6f0abd9067947e467
Ravi Terala <terala@users.noreply.github.com>
2024-05-14 13:50:18 -0700

    User managment - start

--- src/app/routers/webapp.py
+++ src/app/routers/webapp.py
@@ -7,9 +7,19 @@ templates = Jinja2Templates(directory="src/app/templates")
 
 @router.get("/")
 def index(request: Request):
-    return templates.TemplateResponse("home.html", context={"request": request})
+    return templates.TemplateResponse("home", context={"request": request})
 
 
 @router.get("/conversation")
 def conversation(request: Request):
-    return templates.TemplateResponse("conversation.html", context={"request": request})
+    return templates.TemplateResponse("conversation", context={"request": request})
+
+
+@router.get("/user/add")
+def add_user_form(request: Request):
+    return templates.TemplateResponse("add_user_form", context={"request": request})
+
+
+@router.get("/user")
+def list_users(request: Request):
+    return templates.TemplateResponse("list_users", context={"request": request})
